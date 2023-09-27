4b82c3929be690d75e670766c2cad66ef67040f1
Jbod23 <jbodamer23@gmail.com>
2023-09-27 12:06:06 -0700

    ui clean up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root "events#index"
+  root "events#new"
   resources :events
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
